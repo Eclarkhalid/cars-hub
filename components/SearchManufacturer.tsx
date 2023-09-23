6a7756184b9f48bb52b8941629b4cd1159207f64
Eclarkhalid <eclarkhalid@gmail.com>
2023-09-23 21:42:31 +0300

    updating search

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -2,7 +2,7 @@
 
 import { Combobox, Transition } from '@headlessui/react'
 
-import { SearchManufacturerProps } from '@/types'
+import { SearchManuFacturerProps } from '@/types'
 import { useState, Fragment } from 'react'
 import Image from 'next/image'
 
